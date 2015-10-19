f866225c536d3ab72b279c73b30a64c6ff08a1e3
ayeb <ayeb@email.sc.edu>
2015-10-19 11:18:45 -0400

    index on Cube: f970488 Update PowerController.cs

---
+++ Cube/Assets/script/KillZone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillZone : MonoBehaviour {
+
+    public GameObject cube;
+    Vector3 cubePosition = new Vector3(32.71f, 6.9f, 19.4f);
+    void OnTriggerEnter(Collider other)
+    {
+        // destroy all game objects that enter this area
+        Destroy(other.gameObject);
+        //StartCoroutine("RespawnCube");
+        //Application.LoadLevel(Application.loadedLevel);
+      
+
+    }
+    /*IEnumerator RespawnCube()
+    {
+
+        cube = (GameObject)Instantiate(cube, cubePosition, Quaternion.identity) as GameObject;
+        Destroy(cube.gameObject);
+        yield return null;
+    }*/
+
+
+}
