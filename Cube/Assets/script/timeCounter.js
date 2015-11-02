ef20e7d2b7b4d06b5243a801ab709264519f489b
Unlockingcube <Prestis@email.sc.edu>
2015-11-02 10:59:14 -0500

    I have added a timer, also have adjusted my level

---
+++ Cube/Assets/script/timeCounter.js
@@ -0,0 +1,16 @@
+﻿
+var timer;
+timer = 0.0;
+
+
+function Start () {
+
+}
+
+function Update () {
+    timer += Time.deltaTime;
+
+}
+function OnGUI(){
+    GUI.Box(new Rect(10,10,50,20), "" + timer.ToString("0.0"));
+}
