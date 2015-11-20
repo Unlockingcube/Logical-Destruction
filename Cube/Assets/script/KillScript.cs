cbb13e2b0628c9b7ce3d497f4180834087684821
Unlockingcube <Prestis@email.sc.edu>
2015-11-20 10:00:21 -0500

    made some colider changes and rendering on camra for sceen 3.

---
+++ Cube/Assets/script/KillScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillScript : MonoBehaviour {
+    Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+
+        startPos = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter3D(Collider col)
+    {
+        if(col.gameObject.tag == "Kill")
+        {
+            this.transform.position = startPos;
+        }
+    }
+}
