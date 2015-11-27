b90af9b217b1bea019f4f8fbea3783aa5351db1a
mitchem <andrewmitchem70@yahoo.com>
2015-11-27 15:36:55 -0500

    Quest Stuff to make tutorials and directions
    I like an idiot copied over my stuff i was working on and had to go
    through remaking it. going to remake my level over the weekend
    
    the questLog is minorly functional including a questObject to add quest,
    questSteps. needs functionality for incrementing quest and moving quest
    to certain states.
    
    also made the C# timer. it the same as the javascript just with
    different function wrappers.
    
    I need to still make the Respawn controller and objects
    
    all the quest items are now in a subfolder of scripts to help arrange
    them

---
+++ Cube/Assets/script/QuestScripts/QuestLog.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestLog : MonoBehaviour {
+    public string[] questLog = new string[1] { "Welcome: \n F for Ice \n V for Fire \n G For earth \n R for lightning" };
+	public int currentQuest = 0;
+	static public Quest[] questLog2 = new Quest[10];
+	static public int freeSpace = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width/10, Screen.height/10, 100, 100), questLog[currentQuest]);
+        GUI.Label(new Rect(Screen.width / 2, Screen.height / 10, 100, 100), "Teleports: " + PowerController.pc_lightningAmmo);
+        GUI.Label(new Rect(Screen.width / 2, Screen.height / 5, 100, 100)," " +GameObject.Find("Player").GetComponent<PowerController>().power);
+    	if(questLog2[currentQuest]!=null)
+			GUI.Label(new Rect(Screen.width/3, Screen.height/10, 100, 100), questLog2[currentQuest].QuestToString());
+	}
+	void Update () {
+	
+	}
+	void updateCurrentQuest(int target){
+		currentQuest = target;
+	}
+	static public void AddQuest(Quest quest){
+		questLog2 [freeSpace] = quest;
+		freeSpace++;
+	}
+	static public void AddQuest(int target,Quest quest){
+		questLog2 [target] = quest;
+	}
+	static public bool AddQuestStep(string target,QuestStep questStep){
+		int i = 0;
+		while (i < questLog2.Length &&  !questLog2[i].questChain[questLog2[i].currentQuest].text.Equals(target)){
+			i++;
+		}
+		if (i < questLog2.Length) {
+			questLog2 [i].AppendQuestStep (questStep);
+			return true;
+		}
+		else 
+			return false;
+	}
+}
---
+++ Cube/Assets/script/QuestScripts/QuestObject.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestObject : MonoBehaviour {
+	public enum ObjectType{QUEST,DYNAMICSTEP,INCREMENT,QUESTTARGET};
+	public ObjectType whatami;
+	public bool onStart = false;
+	public string questTarget = "";
+	public int target = 0;
+	public QuestStep questStep;
+	public Quest quest;
+	// Use this for initialization
+	void Start () {
+		if (onStart)
+			updateQuestLog ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		updateQuestLog ();
+	}
+	void updateQuestLog(){
+		bool kill = true;
+		switch(whatami){
+		case ObjectType.QUEST:
+			QuestLog.AddQuest(quest);
+			break;
+		case ObjectType.DYNAMICSTEP:
+			kill = QuestLog.AddQuestStep(questTarget,questStep);
+			break;
+		case ObjectType.INCREMENT:
+
+			break;
+		case ObjectType.QUESTTARGET:
+			break;
+		}
+		Debug.Log (kill);
+		if (kill) {
+			gameObject.GetComponent<BoxCollider>().enabled = false;
+			gameObject.GetComponent<MeshRenderer>().enabled = false;
+		}
+
+	}
+}
