18c2541116f03acdffdc8d564de77db964522be9
Unlockingcube <Prestis@email.sc.edu>
2015-11-27 22:24:05 -0500

    I have added a menu that is functional ... uhhhh one thing for some reason the levels are loading in weird as fuck... take a look at it. Seprately loading the scenes works fine, like it always has , but by loading in from the main menu it will start to do funky shit.. like missing textures/coliders, The only scene fully uneffected is scene 3 idk why!

---
+++ Cube/Assets/MainMenu.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function QuitGame() {
+    Debug.Log("game is exiting");
+    Application.Quit();
+}
+function Level1() {
+    Application.LoadLevel ("scene1");
+}
+function Level2() {
+    Application.LoadLevel ("scene2");
+}
+function Level3() {
+    Application.LoadLevel ("scene3");
+}
