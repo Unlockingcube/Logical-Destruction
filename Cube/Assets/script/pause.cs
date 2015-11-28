9f7058b31a09e70b766652a25d3eba265194d478
Unlockingcube <Prestis@email.sc.edu>
2015-11-28 00:03:16 -0500

    trying to get pause menu working...

--- Cube/Assets/script/pause.cs
+++ Cube/Assets/script/pause.cs
@@ -4,67 +4,58 @@ using System.Collections;
 public class pause : MonoBehaviour
 {
 
-    private bool paused = true;
+    private bool paused;
 
     public GUISkin mySkin;
 
     void Start()
     {
-		OutPause ();
-        //you need this to be stated so your game does not start paused. 
+        Time.timeScale = 1;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (!paused)
-            {
-                OnPause();
-            }
-            else
-            {
-                OutPause();
-            }
+            paused = togglePause();
         }
     }
-
-    void OnPause()
-    {
-        Time.timeScale = 0;
-        paused = true;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
-        Debug.Log("IN PAUSE");
-    }
-
-    void OutPause()
-    {
-        Time.timeScale = 1;
-        paused = false;
-        Debug.Log("NOT IN PAUSE");
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+    
 
     void OnGUI()
     {
 
-        if (paused == true)
+        if (Time.timeScale == 0)
         {
             Cursor.visible = true;
-            GUI.Box(new Rect(250, Screen.height - 200, 300, 400), "CIGARETTES BREAK");//"Cigarettes break"
-            if (GUI.Button(new Rect(350, Screen.height - 175, 85, 50), "RESUME"))
+            GUI.Box(new Rect(250, Screen.height - 200, 300, 400), "Whoah bro dis is tuff!");
+            if (GUI.Button(new Rect(350, Screen.height - 175, 85, 50), "Here we go!"))
             {
-                OutPause();
-                Debug.Log("This Should've Unpaused,man. WTF I Quit");
+                togglePause();
             }
-            if (GUI.Button(new Rect(350, Screen.height - 100, 85, 50), "GIVE UP"))
+            if (GUI.Button(new Rect(350, Screen.height - 100, 85, 50), "Im just done!"))
             {
                 Application.Quit();
             }
 
         }
+    }
+    bool togglePause()
+    {
+       if (Time.timeScale == 0)
+       {
+       //    Screen.lockCursor = true;
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 1;
+           return (false);
+       }
+       else
+       {
+            Cursor.lockState = CursorLockMode.Locked;
+       //     Screen.lockCursor = false;
+            Time.timeScale = 0;
+            return (true);
+       }
     }
 
 }
