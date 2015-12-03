7a2dec359f6ed09aef4cd15d1cfa515407f07885
Unlockingcube <Prestis@email.sc.edu>
2015-12-02 22:16:00 -0500

    Added story by pressing k, also adjusted powers as well as worked on fires ability to melt ice. IT works also now have added better pause menu!

--- Cube/Assets/script/pause.cs
+++ Cube/Assets/script/pause.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEditor;
 public class pause : MonoBehaviour
 {
 
@@ -19,25 +19,60 @@ public class pause : MonoBehaviour
         {
             paused = togglePause();
         }
+
     }
     
 
     void OnGUI()
     {
-
+        if (Input.GetKey(KeyCode.K))
+        {
+            //   Debug.Log("TEST K"+ Application.loadedLevelName);
+            if (Application.loadedLevelName.Equals("scene1"))
+            {
+                GUI.Label(new Rect(500, 260f, Screen.height, Screen.width),
+                    "Who am I, what has transpired before today. What has become of who I once was? Will i find my answers in this beautiful place or will i find death? ");
+            }
+            if (Application.loadedLevelName.Equals("scene2"))
+            {
+                GUI.Label(new Rect(500, 260f, Screen.height, Screen.width),
+                    "What is this? I just grabed some cubes and now im here ? why .. this place is dessolate and lacking any color.. . why.. wait .. i just remebered my name! it was cuubee? no Unlockingcube! Thats it! maybe these cubes hold the key to my memory! i must find more! ");
+            }
+            if (Application.loadedLevelName.Equals("scene3"))
+            {
+                Debug.Log("TEST K" + Application.loadedLevelName);
+                GUI.Label(new Rect(500, 260f, Screen.height, Screen.width), "For some reason this feels like the end, and yet i have only just learned my name, what else will i find in the future, maby I had firends, or family. I don't know, but i will find you mark my words!");
+            }
+            if (Application.loadedLevelName.Equals("scene5"))
+            {
+                GUI.Label(new Rect(500, 260f, Screen.height, Screen.width),
+                    "For some reason this place feels like home, I dont know why? but i feel at home here, theres nothing here for me but fun!");
+            }
+        }
         if (Time.timeScale == 0)
         {
             Cursor.visible = true;
-            GUI.Box(new Rect(250, Screen.height - 200, 300, 400), "Whoah bro dis is tuff!");
-            GUI.Label(new Rect(350, Screen.height - 175, 85, 50), "Press E to Resume");
+            GUI.Box(new Rect(300f, 150f,Screen.width/2, Screen.height/2), "Whoah bro dis is tuff!");
+            GUI.Label(new Rect(500,180f, Screen.height,Screen.width), "Press E to Resume");
             if(Input.GetKeyDown(KeyCode.E))
             {
                 togglePause();
             }
-            GUI.Label(new Rect(350, Screen.height - 100, 85, 50), "Press P to Quit");
+            GUI.Label(new Rect(500, 200f, Screen.height, Screen.width), "Press M to Return to menu");
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                Application.LoadLevel("MainMenu");
+            }
+            GUI.Label(new Rect(500, 220f, Screen.height, Screen.width), "Press P to Quit");
             if(Input.GetKeyDown(KeyCode.P))
             {
                 Application.Quit();
+            }
+          
+            GUI.Label(new Rect(500, 240f, Screen.height, Screen.width), "Press N to Restart");
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                Application.LoadLevel(Application.loadedLevel);
             }
 
         }
